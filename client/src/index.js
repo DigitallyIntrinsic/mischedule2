2475c9c0e980f6e041683d3494685085138f3644
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2024-02-08 13:36:58 -0500

    added back a missing line

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'; 
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { decode } from "jwt-decode";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
