71192cdf880eda7e7c8181e7548e0d411ee1353c
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2024-02-08 13:38:44 -0500

    corrected previous location mistake

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,4 +1,4 @@
-import decode from 'jwt-decode';
+import { decode } from "jwt-decode";
 
 class AuthService {
   getProfile() {
@@ -21,23 +21,23 @@ class AuthService {
   isTokenExpired(token) {
     const decoded = decode(token);
     if (decoded.exp < Date.now() / 1000) {
-      localStorage.removeItem('id_token');
+      localStorage.removeItem("id_token");
       return true;
     }
     return false;
   }
 
   getToken() {
-    return localStorage.getItem('id_token');
+    return localStorage.getItem("id_token");
   }
 
   login(idToken) {
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/account');
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/account");
   }
 
   logout() {
-    localStorage.removeItem('id_token');
+    localStorage.removeItem("id_token");
   }
 }
 
